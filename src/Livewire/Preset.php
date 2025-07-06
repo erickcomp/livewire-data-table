d7f33d071d318f54fee8babf42ffe623f9236809
Erick de Azevedo Lima <elima@cecierj.edu.br>
2025-07-06 07:01:12 -0300

    WIP - presets

---
+++ src/Livewire/Preset.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace ErickComp\LivewireDataTable\Livewire;
+
+
+class Preset
+{
+    /**
+     * @param array {
+     *     extends: ?string,
+     *     main-container-class: array<int, string>|array<string, bool>,
+     *     actions-container-class: array<int, string>|array<string, bool>,
+     *     actions-row-class: array<int, string>|array<string, bool>,
+     *     search: array{
+     *         container-class: array<int, string>|array<string, bool>,
+     *         input-class: array<int, string>|array<string, bool>,
+     *         button-class: array<int, string>|array<string, bool>,
+     *         button-use-default-icon: bool
+     *     },
+     *     filters: array{
+     *         collapsible: bool,
+     *         toggle-button-use-default-icon: bool,
+     *         toggle-button-class: array<int, string>|array<string, bool>,
+     *         apply-button-use-default-icon: bool,
+     *         apply-button-container-class: array<int, string>|array<string, bool>,
+     *         apply-button-class: array<int, string>|array<string, bool>,
+     *         item-class: array<int, string>|array<string, bool>,
+     *         content-class: array<int, string>|array<string, bool>,
+     *         range-class: array<int, string>|array<string, bool>,
+     *         select-class: array<int, string>|array<string, bool>,
+     *         input-text-class: array<int, string>|array<string, bool>,
+     *         input-date-class: array<int, string>|array<string, bool>,
+     *         input-datetime-local-class: array<int, string>|array<string, bool>,
+     *         input-number-class: array<int, string>|array<string, bool>
+     *     },
+     *     applied-filters: array{
+     *         container-class: array<int, string>|array<string, bool>,
+     *         label-class: array<int, string>|array<string, bool>,
+     *         applied-filter-item-class: array<int, string>|array<string, bool>,
+     *         button-remove-applied-filter-item-class: array<int, string>|array<string, bool>
+     *     },
+     *     columns-search: array{
+     *         debounce-ms: int,
+     *         columns: mixed // TODO: Add columns configuration
+     *     },
+     *     sorting: array{
+     *         default-sorting-indicators: bool,
+     *         indicator-class: array<int, string>|array<string, bool>,
+     *         indicator-asc-class: array<int, string>|array<string, bool>,
+     *         indicator-desc-class: array<int, string>|array<string, bool>
+     *     },
+     *     table: array<string, mixed>, // TODO: Add table configuration
+     *     pagination: array{
+     *         view: string,
+     *         simple-view: string,
+     *         default-style-for-pagination: bool
+     *     }
+     * }
+     * $presetInfo
+     */
+    public function __construct(
+        array $presetInfo,
+    ) {
+        $presetInfo['applied-filters'][''] ??= [];
+    }
+}
